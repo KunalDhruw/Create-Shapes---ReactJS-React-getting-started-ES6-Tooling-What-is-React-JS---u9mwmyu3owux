daac409d706c39a6128c92c3498507a1a7fa47f5
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-01-15 17:02:21 +0530

    Updated.

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+const handleClick = () => {
+  let b = document.getElementById("select").value;
+  let a = document.createElement("div");
+  if (b === "Square") {
+    a.className = "square";
+  } else {
+    a.className = "circle";
+  }
+  document.getElementById("shapes-holder").appendChild(a);
+};
 const App = () => {
-
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select id="select">
+          <option value="Square">Square</option>
+          <option value="Circle">Circle</option>
+        </select>
+        <button onClick={handleClick}>Add Shape</button>
       </div>
-      <div id="shapes-holder">
-      </div>
+      <div id="shapes-holder"></div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
